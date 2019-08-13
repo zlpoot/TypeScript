ea1b78db15087b5d7e7fad127e20ddd05a39c0b1
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-08-13 10:23:01 -0700

    Original test passes!
    So I'm done, right?

--- tests/cases/conformance/salsa/genericPropertiesOfConstructorFunctions.ts
+++ tests/cases/conformance/salsa/genericPropertiesOfConstructorFunctions.ts
@@ -4,17 +4,29 @@
 // @noEmit: true
 
 /**
- * @constructor
  * @template {string} K
  * @template V
+ * @param {string} ik
+ * @param {V} iv
  */
-function Multimap() {
-    /** @type {Object<string, V>} TODO: Remove the prototype from the fresh object */
+function Multimap(ik, iv) {
+    /** @type {{ [s: string]: V }} */
     this._map = {};
+    // without type annotation
+    this._map2 = { [ik]: iv };
 };
 
-var Ns = {}
-/** @type {Multimap<"a" | "b", number>} */
-const map = new Multimap();
+/** @type {Multimap<"a" | "b", number>} with type annotation */
+const map = new Multimap("a", 1);
+// without type annotation
+const map2 = new Multimap("m", 2);
+
 /** @type {number} */
-const n = map._map['hi']
+var n = map._map['hi']
+/** @type {number} */
+var n = map._map2['hi']
+/** @type {number} */
+var n = map2._map['hi']
+/** @type {number} */
+var n = map._map2['hi']
+
